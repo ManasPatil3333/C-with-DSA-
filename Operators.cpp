ca1ebb8d7eca46fd0b66a893cc653bf7fda4b80c
ManasPatil3333 <patilmanas3333@gmail.com>
2024-03-12 22:58:08 +0530

    new files added

---
+++ Operators.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // 1] Sum of two numbers :
+    /*
+    int num1, num2;
+    cout<<"Enter first number : ";
+    cin>>num1;
+    cout<<"Enter second number : ";
+    cin>>num2;
+    int sum = num1+num2;
+    cout<<"Sum of two numbers is "<<sum<<endl;
+
+    // 2] Swap numbers :
+    
+    int a,b,c;
+    cout<<"Enter two numbers : ";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<a<<"\t"<<b;
+
+    // Floor % Ceil :
+
+    floor(3.14) --> 3
+    ceil(3.14) --> 4
+
+    // Try this :
+    
+    int a = 10;
+    int b = a++;
+    cout<<b<<" ";
+    b = ++a;
+    cout<<a;
+    */
+    return 0;
+}
---
+++ VariablesAndOutput.cpp
@@ -0,0 +1,24 @@
+/*
+--> Rules for naming variables in C++ :
+1] whitespace is not permitted in variable name.
+2] Variable name should not begin with a number.
+3] A C++ keyword cannot be used in variable name.
+4] Essential to use camelcase ( manasPatil ).
+
+--> Points to remember about identifiers :
+1] All identifiers should begin with A to Z or a to z , currency ( $ ) or an underscore ( _ ).
+2] After the first character , identifiers can have any combination of characters.
+3] A keyword cannot be used as keywords.
+4] The identifiers are case sensitive. (Tea and tea both identifiers are different)
+5] White space is not permitted.
+
+--> C++ Data Types :
+Primary :
+Integer , Boolean , Character , Floating Point , Double Floating Point , void , Wide Character .
+Derived :
+Function , Array , Pointer , Reference.
+User-defined :
+Class , Enum , Structure , Typedef , Union.
+
+
+*/
