da70884cc4a8708c47d8d209ac415d1662d45f69
ManasPatil3333 <patilmanas3333@gmail.com>
2024-03-13 23:00:21 +0530

    new file added

--- LoopsProblems.cpp
+++ LoopsProblems.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // 1] Program to count the number of digits of a given number :
+/*
 int main() {
     int number;
     int count = 0;
@@ -13,5 +14,22 @@ int main() {
         count++;
     }
     cout<<number<<" is a "<<count<<" digits number."<<endl;
+    return 0;
+}
+*/
+
+// 2] Program to sum the digits of a given number :
+/**/
+int main() {
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    int sum = 0;
+    while(num>0) {
+        int lastDigit = num%10;
+        sum = sum + lastDigit;
+        num = num/10;
+    }
+    cout<<"Sum : "<<sum<<endl;
     return 0;
 }
