9c1c07366d6d8f2abd10322aec1bfde0b230c49c
ManasPatil3333 <patilmanas3333@gmail.com>
2024-03-13 05:51:06 +0530

    new files added

---
+++ Loops.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+    int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+    int sum = 0;
+    for(int i=0; i<=num; i++) {
+        sum += i;
+    }
+    cout<<"sum of first "<<num<<" natural numbers is "<<sum<<"."<<endl;
+    return 0;
+}
+
+int main() {
+    int maxRange;
+    cout<<"Enter the range for getting case solution : ";
+    cin>>maxRange;
+    for(int i=0; i<=maxRange; i++) {
+        if(i%5 == 0 && i%7 == 0) {
+            cout<<i<<"\t";
+        }
+    }
+    return 0;
+}
+*/
+int main() {
+    for(int i=1; i<=50; i++) {
+        if( i%3 != 0) {
+            cout<<i<<"\t";
+        }
+    }
+    return 0;
+}
---
+++ Statements.cpp
@@ -0,0 +1,47 @@
+#include <iostream> 
+using namespace std;
+/*
+int main() {
+    int num;
+    cout<<"Enter a number from 1 to 7 : ";
+    cin>>num;
+    switch(num) {
+        case 1 :
+        cout<<"Monday"<<endl;
+        break;
+        case 2 :
+        cout<<"Tuesday"<<endl;
+        break;
+        case 3 :
+        cout<<"Wednesday"<<endl;
+        break;
+        case 4 :
+        cout<<"Thursday"<<endl;
+        break;
+        case 5 :
+        cout<<"Friday"<<endl;
+        break;
+        case 6 :
+        cout<<"Saturday"<<endl;
+        break;
+        case 7 :
+        cout<<"Sunday"<<endl;
+        break;
+        default :
+        cout<<"Invalid input data.";
+    }
+    return 0;
+}
+
+int main() {
+    int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+    if(num%2 == 0 && num%3 == 0) {
+        cout<<num<<" is a chosen one!"<<endl;
+    } else {
+        cout<<num<<" is not a cool number, preety disappointing!"<<endl;
+    }
+    return 0;
+}
+*/
